9aa86ddcac2fa0e84bf09cd23c078d237772ee4f
¨pia <granieripia@gmail.com>
2023-12-03 20:44:08 -0300

    mas detalles entrega final

--- js/terapias.js
+++ js/terapias.js
@@ -29,8 +29,13 @@
 // terapiasYprecios.forEach((terapia)=>{
 //     console.log(terapia.nombre)
 // })
-let nombreUsuario
-let edadUsuario
+
+
+
+
+
+// let nombreUsuario
+// let edadUsuario
 
 function bienvenida(){
     nombreUsuario = prompt("Ingrese su nombre completo");
@@ -42,32 +47,40 @@ function bienvenida(){
     }
 }
 
-bienvenida();
-console.log("Nombre: "+ nombreUsuario + ". Edad: " + edadUsuario)
+// bienvenida();
+// console.log("Nombre: "+ nombreUsuario + ". Edad: " + edadUsuario)
 
-const terapia = function(precio, tipo, extra, precioad){
-    this.precio = precio;
-    this.tipo = tipo;
-    this.extra = extra;
-    this.precioad = precioad;
+// const terapia = function(precio, tipo, extra, precioad){
+//     this.precio = precio;
+//     this.tipo = tipo;
+//     this.extra = extra;
+//     this.precioad = precioad;
+// }
+
+// // variables de objetos
+// let astrologia = new terapia (10000, "Carta Natal", "Revolución Solar", 3000)
+// let reiki = new terapia (5000, "Individual")
+// let constelaciones = new terapia (12000, "Grupal")
+// let constelaciones2 = new terapia (15000, "Individual")
+// let tarot = new terapia (5000, "Tirada completa", "Tres preguntas", 2000)
+// let radiestesia = new terapia (8000, "Limpieza", "Armonización", 3000)
+// let numerologia = new terapia (6000, "Lectura clasica", "Anual por cumpleaños", 4000)
+// let registros = new terapia (6000, "Tres preguntas", "Tres preguntas más", 3000)
+// let tFloral = new terapia (6000, "Sesión + frasco", "Frasco grande", 1500)
+
+// //array terapias
+
+// const Terapias = [astrologia, reiki, constelaciones, constelaciones2, tarot, radiestesia, numerologia, registros, tFloral]
+
+// function terapiaEleccion (){
+//     let Eleccion = prompt("Elegí que terapia querés realizar").toLowerCase();
+
+// }
+
+let botones = document.getElementsByClassName("btn btn-outline-dark")
+
+for (let i = 0; i < botones.length; i++) {
+    botones[i].addEventListener("click", bienvenida);
 }
 
-// variables de objetos
-let astrologia = new terapia (10000, "Carta Natal", "Revolución Solar", 3000)
-let reiki = new terapia (5000, "Individual")
-let constelaciones = new terapia (12000, "Grupal")
-let constelaciones2 = new terapia (15000, "Individual")
-let tarot = new terapia (5000, "Tirada completa", "Tres preguntas", 2000)
-let radiestesia = new terapia (8000, "Limpieza", "Armonización", 3000)
-let numerologia = new terapia (6000, "Lectura clasica", "Anual por cumpleaños", 4000)
-let registros = new terapia (6000, "Tres preguntas", "Tres preguntas más", 3000)
-let tFloral = new terapia (6000, "Sesión + frasco", "Frasco grande", 1500)
-
-//array terapias
-
-const Terapias = [astrologia, reiki, constelaciones, constelaciones2, tarot, radiestesia, numerologia, registros, tFloral]
-
-function terapiaEleccion (){
-    let Eleccion = prompt("Elegí que terapia querés realizar").toLowerCase();
-
-}
+botones.addEventListener = ("click", bienvenida)
