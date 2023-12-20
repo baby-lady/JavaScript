01998aeb64d30460babf119ff3853babf6765ed4
¨pia <granieripia@gmail.com>
2023-12-20 17:02:57 -0300

    tienda

--- js/terapias.js
+++ js/terapias.js
@@ -1,11 +1,16 @@
 
-function bienvenida(){
+function Edad(){
     edadUsuario = parseInt(prompt("Ingrese su edad"));
-    edadUsuario > 18 ? alert("¡Bienvenido!") : alert("Debés ser mayor para continuar.");
+    edadUsuario > 18 ? console.log("El usuario es mayor de edad") : console.log("El usuario es menor de edad");
     if (edadUsuario >= 18){
         window.location.href = "compras.html";
     } else {
-        alert("Usted no tiene permitido el acceso");
+        // alert("Usted no tiene permitido el acceso");
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "No tenés permito el acceso",
+        });
     }
 }
 
@@ -14,10 +19,10 @@ function bienvenida(){
 let botones = document.getElementsByClassName("btn btn-outline-dark")
 
 for (let i = 0; i < botones.length; i++) {
-    botones[i].addEventListener("click", bienvenida);
+    botones[i].addEventListener("click", Edad);
 }
 
-botones.addEventListener = ("click", bienvenida)
+botones.addEventListener = ("click", Edad);
 
 
 // creación de objetos de distintas terapias
@@ -33,56 +38,22 @@ const terapia = function(nombre, precio, tipo, extra, precioad, img){
 
 // variables de objetos de terapias
 
-let astrologia = new terapia ("Astrología", 10000, "Carta Natal", "Revolución Solar", 3000, "../assets/icons/zodiac.png")
-let reiki = new terapia ("Reiki", 5000, "Individual", "", "", "../assets/icons/reiki.png")
-let constelaciones = new terapia ("Constelaciones Familiares", 12000, "Grupal", "", "", "../assets/icons/constelaciones.png")
-let tarot = new terapia ("Tarot", 5000, "Tirada completa", "Tres preguntas", 2000, "../assets/icons/taroh.png")
-let radiestesia = new terapia ("Radiestesia", 8000, "Limpieza", "Armonización", 3000, "../assets/icons/radiestesia.png")
-let numerologia = new terapia ("Numerología", 6000, "Lectura clasica", "Anual por cumpleaños", 4000, "../assets/icons/numerologia.png")
-let registros = new terapia ("Registros Akashicos", 6000, "Tres preguntas", "Tres preguntas más", 3000, "../assets/icons/akasha.png")
-let tFloral = new terapia ("Terapia Floral de Bach", 7000, "Sesión + frasco", "Frasco grande", 1500, "../assets/icons/flores.png")
+let astrologia = new terapia ("Astrología", 10000, "Carta Astral + Revolucion Solar. Duración: 2:30hs", "Revolución Solar", 3000, "../assets/icons/zodiac.png")
+let reiki = new terapia ("Reiki", 5000, "Sesiones individuales. Duración: 1hs", "", "", "../assets/icons/reiki.png")
+let constelaciones = new terapia ("Constelaciones Familiares", 12000, "Sesión grupal (grupos de 5/6 personas). Duración: 4hs", "", "", "../assets/icons/constelaciones.png")
+let tarot = new terapia ("Tarot", 5000, "Retrato actual del consultante. Duración: 1hs", "Tres preguntas", 2000, "../assets/icons/taroh.png")
+let radiestesia = new terapia ("Radiestesia", 8000, "Armonización y limpieza de contaminantes energéticos. Duración: 1:30hs", "Armonización", 3000, "../assets/icons/radiestesia.png")
+let numerologia = new terapia ("Numerología", 6000, "Lectura clásica anual (de cumpleaños). Duración: 1hs", "Anual por cumpleaños", 4000, "../assets/icons/numerologia.png")
+let registros = new terapia ("Registros Akashicos", 6000, "Devolución de cinco preguntas (Se solicitan previamente). Duración: 1:30hs", "Tres preguntas más", 3000, "../assets/icons/akasha.png")
+let tFloral = new terapia ("Terapia Floral de Bach", 7000, "Sesión individual, incluye frasco 30ml. Duración: 1hs", "Frasco grande", 1500, "../assets/icons/flores.png")
 
 //array terapias
 
 const Terapias = [astrologia, reiki, constelaciones, tarot, radiestesia, numerologia, registros, tFloral]
 
 const terapiasEnJSON = JSON.stringify(Terapias)
-localStorage.setItem("terapias a elección", terapiasEnJSON)
+localStorage.setItem("Listado de terapias", terapiasEnJSON)
 
-// PÁGINA ÚNETE; form
-
-// const inputNombre = document.getElementById("nombre");
-// const inputApellido = document.getElementById("apellido");
-// const inputMail = document.getElementById("mail");
-// const inputFecha = document.getElementById("fecha-de-nacimiento")
-// const inputLugar = document.getElementById("lugar");
-// const inputHora = document.getElementById("hora");
-// const botonEnviar = document.getElementById("enviar");
-
-// function guardarForm (){
-//     const datosdelForm ={
-//         nombre:inputNombre.value,
-//         apellido:inputApellido.value,
-//         mail:inputMail.value,
-//         fecha:inputFecha.value,
-//         lugar:inputLugar.value,
-//         hora:inputHora.value,
-//     }
-//     let resultado=JSON.stringify(datosdelForm);
-//     localStorage.setItem("datosForm", resultado);
-//     console.log("datos ingresados por el usuario: " + resultado);
-// }
-
-// botonEnviar.addEventListener("click", guardarForm);
-
-// function cargarForm(){
-//     const formulario=document.getElementById("Form"),
-//     const DatosJSON=localStorage.getItem("datosForm"),
-//     if (DatosJSON){
-//         const datosdelForm=JSON.parse(datosdelForm),
-//         formulario.nombre.value=datosdelForm.nombre,
-//     }
-// }
 
 // TIENDA
 
@@ -103,19 +74,26 @@ let carrito = []
 
 Terapias.forEach((recorrido) => {
     let contenido = document.createElement("div");
-    //contenido.classname = "cardTerapia";
+    contenido.className = "cardsCompras"
     contenido.innerHTML = `
     <img src=${recorrido.img}>
-    <h3>${recorrido.nombre}</h3>
+    <h2>${recorrido.nombre}</h2>
+    <p>${recorrido.tipo}
     <p>$${recorrido.precio}</p>
     `;
     Tienda.append(contenido);
 
     let compras = document.createElement("button");
     compras.innerText = "Agregar al carrito";
-    //compras.className = "comprar";
-
+    compras.className = "comprar";
     contenido.append(compras);
-    
+
+    compras.addEventListener("click", () => {
+        carrito.push({
+            nombre : recorrido.nombre,
+            precio : recorrido.precio,
+        })
+        console.log (carrito)
+    })
 });
 
